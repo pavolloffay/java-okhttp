d308ff89113388ffaf58ed2005d7318991e8242d
Pavol Loffay <ploffay@redhat.com>
2017-09-12 09:19:49 +0200

    Example passing additional headers

--- opentracing-okhttp3/src/test/java/io/opentracing/contrib/okhttp3/TracingCallFactoryTest.java
+++ opentracing-okhttp3/src/test/java/io/opentracing/contrib/okhttp3/TracingCallFactoryTest.java
@@ -3,8 +3,13 @@ package io.opentracing.contrib.okhttp3;
 import java.io.IOException;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.RecordedRequest;
 
 /**
  * @author Pavol Loffay
@@ -19,5 +24,23 @@ public class TracingCallFactoryTest extends AbstractOkHttpTest {
     public void before() throws IOException {
         super.before();
 
+    }
+
+    @Test
+    public void testPassingHeaders() throws IOException, InterruptedException {
+        mockWebServer.enqueue(new MockResponse()
+                .setResponseCode(202));
+
+        Request.Builder builder = new Request.Builder().url(mockWebServer.url("/foo"));
+        builder.addHeader("X-Direct", "Foo");
+
+        RequestBuilderInjectAdapter injectAdapter = new RequestBuilderInjectAdapter(builder);
+        injectAdapter.put("X-B3-Foo", "Bar");
+
+        Response response = client.newCall(builder.build()).execute();
+        System.out.println(response.code());
+        RecordedRequest recordedRequest = mockWebServer.takeRequest();
+        System.out.println(recordedRequest.getHeader("X-B3-Foo"));
+        System.out.println(recordedRequest.getHeader("X-Direct"));
     }
 }
--- opentracing-okhttp3/src/test/java/io/opentracing/contrib/okhttp3/TracingInterceptorTest.java
+++ opentracing-okhttp3/src/test/java/io/opentracing/contrib/okhttp3/TracingInterceptorTest.java
@@ -1,10 +1,16 @@
 package io.opentracing.contrib.okhttp3;
 
+import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
 import org.junit.Ignore;
+import org.junit.Test;
 
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.RecordedRequest;
 
 /**
  * @author Pavol Loffay
@@ -18,5 +24,23 @@ public class TracingInterceptorTest extends AbstractOkHttpTest {
     @Ignore("Does not work for interceptors")
     @Override
     public void testAsyncMultipleRequests() throws ExecutionException, InterruptedException {
+    }
+
+    @Test
+    public void testPassingHeaders() throws IOException, InterruptedException {
+        mockWebServer.enqueue(new MockResponse()
+                .setResponseCode(202));
+
+        Request.Builder builder = new Request.Builder().url(mockWebServer.url("/foo"));
+        builder.addHeader("X-Direct", "Foo");
+
+        RequestBuilderInjectAdapter injectAdapter = new RequestBuilderInjectAdapter(builder);
+        injectAdapter.put("X-B3-Foo", "Bar");
+
+        Response response = client.newCall(builder.build()).execute();
+        System.out.println(response.code());
+        RecordedRequest recordedRequest = mockWebServer.takeRequest();
+        System.out.println(recordedRequest.getHeader("X-B3-Foo"));
+        System.out.println(recordedRequest.getHeader("X-Direct"));
     }
 }
