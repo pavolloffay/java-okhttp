05946ce811b338fe034acceb5fed94a54a4385a0
Pavol Loffay <ploffay@redhat.com>
2017-10-11 17:29:04 +0200

    Bump OT to 0.31.0-RC1

--- opentracing-okhttp3/src/main/java/io/opentracing/contrib/okhttp3/TracingInterceptor.java
+++ opentracing-okhttp3/src/main/java/io/opentracing/contrib/okhttp3/TracingInterceptor.java
@@ -1,5 +1,6 @@
 package io.opentracing.contrib.okhttp3;
 
+import io.opentracing.Scope;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -7,9 +8,7 @@ import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.logging.Logger;
 
-import io.opentracing.ActiveSpan;
 import io.opentracing.Tracer;
-import io.opentracing.contrib.okhttp3.concurrent.TracingExecutorService;
 import io.opentracing.tag.Tags;
 import okhttp3.Dispatcher;
 import okhttp3.Interceptor;
@@ -77,26 +76,26 @@ public class TracingInterceptor implements Interceptor {
 
         // application interceptor?
         if (chain.connection() == null) {
-            ActiveSpan span = tracer.buildSpan(chain.request().method())
+            Scope scope = tracer.buildSpan(chain.request().method())
                     .withTag(Tags.COMPONENT.getKey(), TracingCallFactory.COMPONENT_NAME)
-                    .startActive();
+                    .startActive(true);
 
             Request.Builder requestBuilder = chain.request().newBuilder();
 
             Object tag = chain.request().tag();
             TagWrapper tagWrapper = tag instanceof TagWrapper
                     ? (TagWrapper) tag : new TagWrapper(tag);
-            requestBuilder.tag(new TagWrapper(tagWrapper, span));
+            requestBuilder.tag(new TagWrapper(tagWrapper, scope.span()));
 
             try {
                 response = chain.proceed(requestBuilder.build());
             } catch (Throwable ex) {
                 for (OkHttpClientSpanDecorator spanDecorator: decorators) {
-                    spanDecorator.onError(ex, span);
+                    spanDecorator.onError(ex, scope.span());
                 }
                 throw ex;
             } finally {
-                span.deactivate();
+                scope.close();
             }
         } else {
             Object tag = chain.request().tag();
